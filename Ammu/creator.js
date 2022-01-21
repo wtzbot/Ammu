8abafaf94fe772b3d764f429711f1fa414ab0941
I_AM_DEVIL <88338865+D-E-V-I-L-8@users.noreply.github.com>
2022-01-21 21:40:43 +0530

    `AMMU WHATSAPP-BOT`

--- Ammu/creator.js
+++
@@ -1,9 +0,0 @@
-function handler(m) {
-  this.sendContact(m.chat, global.owner[0], this.getName(global.owner[0] + '@s.whatsapp.net'), m)
-}
-handler.help = ['owner', 'creator']
-handler.tags = ['info']
-
-handler.command = /^(owner|creator)$/i
-
-module.exports = handler
--- Ammu/sendquote.js
+++
@@ -1,8 +0,0 @@
-async function handler(m) {
-    if (!m.quoted) throw 'reply message!'
-    let q = this.serializeM(await m.getQuotedObj())
-    if (!q.quoted) throw 'the message you are replying to contains no reply!'
-    await q.quoted.copyNForward(m.chat, true)
-}
-handler.command = /^q$/i
-module.exports = handler
