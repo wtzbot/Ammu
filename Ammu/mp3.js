ce65cfd19495643b6d338f19cb00d7338b556d6f
I_AM_DEVIL <88338865+D-E-V-I-L-8@users.noreply.github.com>
2022-02-06 18:17:20 +0530

    Update mp3.js

--- Ammu/mp3.js
+++ Ammu/mp3.js
@@ -3,7 +3,7 @@ const { toAudio } = require('../lib/converter')
 let handler = async (m, { conn, usedPrefix, command }) => {
   let q = m.quoted ? m.quoted : m
   let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-  if (!/video|audio/.test(mime)) throw `Reply to the video or voice note that you want to convert to mp3 with the command *${usedPrefix + command}*`
+  if (!/video|audio/.test(mime)) throw `ʀᴇᴘʟʏ ᴛᴏ ᴛʜᴇ ᴠɪᴅᴇᴏ ᴏʀ ᴠᴏɪᴄᴇ ɴᴏᴛᴇ ᴛʜᴀᴛ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴄᴏɴᴠᴇʀᴛ ᴛᴏ ᴍᴘ3`
   let media = await q.download()
   let audio = await toAudio(media, 'mp4')
   conn.sendFile(m.chat, audio, '', '', m, 0, { mimetype: 'audio/mp4' })
